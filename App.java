a312c7dec7cebeee43187adcd008cb7a15be299d
Rjchavan3499 <59140623+Rjchavan3499@users.noreply.github.com>
2021-12-01 20:40:57 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,24 @@
+package Bridgelabz.sample1;
+ 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String message="hello world";
+        LOG.debug(message+"hejeiik");
+        LOG.info(message+"hiii");
+        	
+        
+    }
+}
